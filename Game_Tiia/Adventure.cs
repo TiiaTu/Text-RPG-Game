393b69d71a63823cd0a136e3c8504ba3d30ae389
Tiia Tupamäki <tiia.tupamaki@gmail.com>
2021-10-24 17:22:24 +0200

    Lagt till grejer i menun

---
+++ Game_Tiia/Adventure.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Game_Tiia
+{
+    class Adventure
+    {
+        public static void WhileOnAdventure()
+        {
+            Random rand = new Random();
+            
+        }
+    }
+}
--- Game_Tiia/Program.cs
+++ Game_Tiia/Program.cs
@@ -10,38 +10,67 @@ namespace Game_Tiia
         static void Main(string[] args)
         {
             MainHeader();
-            bool gameOn = true;
+            Player player = new Player();
             
-            while(gameOn)
-            {
-                ShowMainMenu();
+            AskForUsername();           
+            player.Name= Console.ReadLine().ToString();
+            Console.ResetColor();
+
+            bool gameOn = true;
+
+            while (gameOn)
+            {                             
+                Console.WriteLine("\n 1. Go adventuring");
+                Console.WriteLine(" 2. Show details about your character");
+                Console.WriteLine(" 3. Exit Game\n");
+                Console.Write(">> ");
+
+                int menuChoise = 0;
+                //Console.ForegroundColor = ConsoleColor.Cyan;
+                var input = int.TryParse(Console.ReadLine(), out menuChoise);
+                //Console.ResetColor();
+
+                switch (menuChoise)
+                {
+                    case 1: Adventure.WhileOnAdventure(); break;
+                    case 2: ShowDetails(player.Name); break;
+                    case 3: Environment.Exit(0); break;
+
+                    default:
+                        break;
+                }
             }
         }
 
-        private static void ShowMainMenu()
+        public static void MainHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("╒═══════════════════════════════╕");
+            Console.WriteLine("╞          -THE GAME-           ╡ ");
+            Console.WriteLine("╘═══════════════════════════════╛");
+            Console.ResetColor();
+        }
+
+        public static void AskForUsername() //Frågar om användarnamnet
         {
             Console.Write("Enter your username: ");
             Console.ForegroundColor = ConsoleColor.Cyan;
-            var username = Console.ReadLine().ToString();
-            Console.ResetColor();
-         
-            Console.WriteLine("\n 1. Go adventuring");
-            Console.WriteLine(" 2. Show details about your character");
-            Console.WriteLine(" 3. Exit Game\n");
-            Console.Write(">> ");
-            
-            int menuChoise=0;
-            var input = int.TryParse(Console.ReadLine(), out menuChoise);
+        }
 
-            switch (menuChoise)
-            {
-                case 1: break;
-                case 2: break;
-                case 3: Environment.Exit(0); break;
-                    
-                default:
-                    break;
-            }
+        private static void ShowDetails(string username)
+        {
+            Console.WriteLine("═════════════════════════");
+            Console.WriteLine($"Name:       {username}");
+            Console.WriteLine($"Level:      "); //TODO: lägg till level här
+            Console.WriteLine($"HP:      /200"); //TODO: lägg till hp
+            Console.WriteLine($"Exp:     /100");
+            Console.WriteLine($"Gold:      ");
+            Console.WriteLine($"Strenght:  ");
+            Console.WriteLine($"Toughness: ");
+            Console.WriteLine("═════════════════════════");
+            Console.WriteLine("Press any key to go back to MENU");
+            Console.ReadLine();
+            Console.Clear();
         }
 
         public static void Attack()
@@ -50,11 +79,6 @@ namespace Game_Tiia
             //int strenghtLost = random.Next(, )
         }
 
-        public static void MainHeader()
-        {
-            Console.WriteLine("╒═══════════════════════════════╕");
-            Console.WriteLine("╞          -THE GAME-           ╡ ");
-            Console.WriteLine("╘═══════════════════════════════╛");
-        }
+        
     }
 }
